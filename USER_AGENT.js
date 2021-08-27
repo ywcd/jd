e00565af720a7688815dc4c59e629039df3910e4
刘庆 <lq510885@qq.com>
2021-08-27 20:13:37 +0800

    Update

--- USER_AGENT.js
+++ USER_AGENT.js
@@ -48,7 +48,7 @@ function randomNumber(min = 0, max = 100) {
 const RANDOM_UA = USER_AGENTS[randomNumber(0, USER_AGENTS.length)];
 
 const GET_RANDOM_TIME_UA = () => {
-  return 'Mozilla/5.0 (Linux; Android 10; DT2002C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.156 Mobile Safari/537.36';
+  return 'Mozilla/5.0 (Linux; U; Android 10; zh-CN; DT2002C Build/QKQ1.200712.002) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.108 UCBrowser/13.5.3.1133 Mobile Safari/537.36';
 };
 
 module.exports = {
