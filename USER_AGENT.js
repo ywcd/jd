9b346093462b79833f6a85576878fca8d9530c70
刘庆 <lq510885@qq.com>
2021-08-27 19:49:59 +0800

    .env

--- USER_AGENT.js
+++ USER_AGENT.js
@@ -48,7 +48,7 @@ function randomNumber(min = 0, max = 100) {
 const RANDOM_UA = USER_AGENTS[randomNumber(0, USER_AGENTS.length)];
 
 const GET_RANDOM_TIME_UA = () => {
-  return 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/92.0.4515.90 Mobile/15E148 Safari/604.1';
+  return 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X; zh-cn) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/19A5337a UCBrowser/5.2.8.1180 Mobile';
 };
 
 module.exports = {
